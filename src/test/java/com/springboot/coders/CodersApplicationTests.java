d9c3d0414ccd23d5e63a2b9c3122c1d9d34edfbc
Krishna Torrem <krishnatorrem@gmail.com>
2025-03-19 00:05:00 +0530

    init

---
+++ src/test/java/com/springboot/coders/CodersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.coders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
